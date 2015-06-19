0a6f15787c73d76cee6f6fb036cec2aee6db1eac
eviera@sdd <eviera@sdd>
2015-06-19 18:57:32 -0300

    vista de articulos

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,7 +14,22 @@ angular
         templateUrl: 'views/wiki.html',
         controller: 'wikiCtrl'
       })
+      .when('/article/:id', {
+        templateUrl: 'views/article.html',
+        controller: 'articleCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-});
+
+  })
+  .factory('ArticleRestangular', function(Restangular) {  //Para reemplazar los IDs
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+  .factory('ArticleService', function(ArticleRestangular) {
+    return ArticleRestangular.service('rest/article'); //apunta a la url /rest/article
+  });
--- client/scripts/controllers/wiki.js
+++ client/scripts/controllers/wiki.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('wikimeanApp')
-  .controller('wikiCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('wikiCtrl', function ($scope, ArticleService) {
+  	$scope.articles = ArticleService.getList().$object; //Hace la llamada a getList, y cuando vuelve, copia las cosas a $object... y de alguna manera se le asigna a $scope.articles
   });
