1a32bd3bce3e50417e053f1bd50e7d7555decf18
eviera@asterix <eviera@asterix>
2015-06-21 23:59:18 -0300

    campo fecha

--- server/models/Article.js
+++ server/models/Article.js
@@ -1,14 +1,10 @@
 var mongoose = require('mongoose');
 
 var ArticleSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  }
+  title:    { type: String, required: true },
+  content:  { type: String, required: true },
+  created:  { type: Date, required: true, default: Date.now },
+  modified: { type: Date, required: true }
 });
 
 module.exports = ArticleSchema;
