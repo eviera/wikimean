48af36860628fff335253a2717ba567d82b90c3f
emi@ideafix <eviera@ideafix>
2015-07-15 00:36:32 -0300

    migracion de articulos

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ ToDo
 
 
 Links interesantes:
+http://txstyle.org/
 https://github.com/borgar/textile-js
 https://scotch.io/tutorials/setting-up-a-mean-stack-single-page-application
 https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
--- client/scripts/controllers/article.js
+++ client/scripts/controllers/article.js
@@ -2,12 +2,15 @@
 
 angular.module('wikimeanApp')
   .controller('articleCtrl', function ($scope, $routeParams, ArticleRestangular) {
+    $scope.alert = {};
 
     ArticleRestangular.one('article', $routeParams.id).get().then(function(article) {
+        $scope.alert.show = false;
         $scope.article = article;
         $scope.saveArticle = function () {
           $scope.article.save().then(function () {
-            alert('guardado' + $scope.article.content);
+            $scope.alert.show = true;
+            $scope.alert.message = "Article saved";
           });
         };
     });
